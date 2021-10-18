2d831f529f10b639de0259edce9a835377b116f3
Leonardo Loureiro <leolou.90@gmail.com>
2021-10-18 05:13:10 -0700

    get info

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ function getWeather(){
     fetch(url).then(
         (response) => response.json()
     ).then(
-        (data) => console.log(data)
+        function(data){
+            const {name: location, weather,main:{temp:temp }, sys:{country:country}} = data;
+            /*console.log(location);
+            console.log(weather);
+            console.log(icon);
+            console.log(temp);
+            console.log(country);*/
+            displayData(location, weather, temp, country);
+        }
     );
+}
+
+function displayData(location, weather, temp, country){
+    //Access the DOM elements to be updated
+    let grades = document.getElementsByClassName("grades");
+    let city = document.getElementsByClassName("location");
+    let place = document.getElementsByClassName("country");
+    let icon = document.getElementsByClassName("weatherIcon");
+    let conditions = document.getElementsByClassName("weatherConditions");
+
+    city[0].innerHTML = location;
+    console.log(weather[2], weather[3]);
+    conditions[0].innerHTML = weather[2];
+    icon.src = "http://openweathermap.org/img/wn/"+weather[3]+ "@2x.png"
+    grades[0].innerHTML = Math.floor(temp)  + "&#176;";
+    place[0].innerHTML = country;
 }
